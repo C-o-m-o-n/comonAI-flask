90e9baf7dd2422b1a3c7177186537faf256bb5ab
C-o-m-o-n <comon928@gmail.com>
2023-03-10 13:22:55 +0300

    changed template

--- main.py
+++ main.py
@@ -1,35 +1,97 @@
 from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 import openai
+from flask_socketio import SocketIO, send, emit
 
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 app = Flask(__name__)
 
-@app.route('/' ,methods=['POST', 'GET'])
+# sqlite database
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SECRET_KEY']='sqlite:///database'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+class HumanChats(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date_sent = db.Column(db.DateTime)
+  content = db.Column(db.Text)
+  # chat_img = db.Column(db.String(100))
+  #foreign key to link to other tables
+  chater_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  response_content = db.relationship('ChatGptChats', backref='gpt')
+
+  
+class ChatGptChats(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date_sent = db.Column(db.DateTime)
+  gpt_response = db.Column(db.Text)
+  # chat_img = db.Column(db.String(100))
+  #foreign key to link to other tables
+  human_chat_id = db.Column(db.Integer, db.ForeignKey('human_chats.id'))
+  
+#define the Users table
+class Users(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(250))
+  email = db.Column(db.String(250))
+  password = db.Column(db.String(256))
+  #profile_pic = db.Column(db.String(250), default='user.png')
+  #pic_file_path = db.Column(db.String(256))
+  date_joined = db.Column(db.DateTime)
+  #user can have many chats
+  chat = db.relationship('HumanChats', backref='chater')
+    
+  
+@app.route('/')
 def index():
-    form_prompt = 'hi'
-    if request.method == 'POST':
-        form_prompt = request.form['prompt']
+  start_sequence = "\nAI:"
+  restart_sequence = "\nHuman: "
+  #user = Users(username='john', email='john@gmail.com', password='12345678', date_joined=datetime.now())
+  #db.create_all()
+  #db.session.add(user)
+  #db.session.commit()
 
-        start_sequence = "\nAI:"
-        restart_sequence = "\nHuman: "
+  human_chats = HumanChats.query.all()
+  back_gpt = ChatGptChats.query.all()
+  
+  return render_template("index.html", human_chats=human_chats, back_gpt=back_gpt)
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=form_prompt,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"]
+#events handles
+@socketio.on('message')
+def message(data):
+  print(data['msg'])
+  #response from gpt
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=data["msg"],
+    temperature=0.9,
+    max_tokens=20,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
         )
-    print(form_prompt)
-    return render_template("index.html", response=response)
-
-
+  
+  gpt_chats = ChatGptChats(date_sent=datetime.now(), gpt_response=response["choices"][0]["text"])
+  print('response@ ',response["choices"][0]["text"])
+  db.create_all()
+  db.session.add(gpt_chats)
+  db.session.commit()
+  
+  emit('gpt-response', response["choices"][0]["text"])
+  send({'msg':data['msg']})
+  
+  human_chats = HumanChats(date_sent=datetime.now(), content=data["msg"])
+  db.create_all()
+  db.session.add(human_chats)
+  db.session.commit()
+  
+  return data
+  #sends the message to event called message
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  socketio.run(app, debug=True)
---
+++ static/scripts.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  var socket = io.connect('http://' + document.domain + ':'+ location.port);
+  
+  // socket.on('connect', function(){
+  //   socket.send({'msg': 'successfully connected'})
+  //   console.log('connected')
+  // }
+    //)
+  //sends the message to the server
+  var messageButton = document.querySelector('#send-btn');
+  var messageInput = document.querySelector('#message-input');
+  messageButton.onclick = function(e){
+    
+    socket.send({'msg': messageInput.value});
+    messageInput.value = ''
+  };
+  let directMessage = document.querySelector('.direct-chat-messages');
+  //reciece message from the server
+  socket.on('message', function(data){
+      const div = document.createElement('div');
+      div.setAttribute('class', 'direct-chat-text')
+      const p = document.createElement('p');
+      //const span_timestamp = document.createElement('small');
+      const br = document.createElement('br');
+      const hr = document.createElement('hr');
+      //span_timestamp.innerHTML = data.time_stamp;
+      //span_timestamp.class = "msg_time";
+      p.innerHTML =  data.msg ;
+      div.innerHTML = p.outerHTML
+      var messageDisplay = document.querySelector('#message-display');
+      var timeDisplay = document.querySelector('.direct-chat-timestamp');
+      //messageDisplay.append(p);
+      directMessage.append(div)
+      
+    }
+  );
+  //recv gpt response
+  socket.on('gpt-response', (data)=>{
+    const div = document.createElement('div');
+    
+    const right = document.createElement('div');
+    right.setAttribute('class', 'right','direct-chat-msg')
+    
+    const info = document.createElement('div');        
+    info.setAttribute('class', 'direct-chat-info clearfix')
+    
+    const name = document.createElement('span')
+    name.setAttribute('class', 'direct-chat-name', 'pull-right')
+    
+    const time = document.createElement('span')
+    time.setAttribute('class', 'direct-chat-timestamp', 'pull-left')
+    
+    const text = document.createElement('div')
+    text.setAttribute('class', 'direct-chat-text')
+
+    div.setAttribute('class', 'direct-chat-text')
+    const p = document.createElement('p');
+    //const span_timestamp = document.createElement('small');
+    const br = document.createElement('br');
+    const hr = document.createElement('hr');
+    //span_timestamp.innerHTML = data.time_stamp;
+    //span_timestamp.class = "msg_time";
+    p.innerHTML =  data;
+    text.innerHTML = p.outerHTML
+    
+    info.innerHTML= name.outerHTML + time.outerHTML
+    right.innerHTML = info.outerHTML + text.outerHTML
+
+    var gptMessageDisplay = document.querySelector('#gpt-message-display');
+    var gptTimeDisplay = document.querySelector('#gpt-timestamp');
+    //gptMessageDisplay.append(p);
+    directMessage.append(right)
+  })
+
+  });
+
+
+//const right = document.createElement('div');        
+//right.setAttribute('class', 'right','direct-chat-msg')
+
