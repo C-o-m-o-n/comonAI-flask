4f572b25124c5edffb114caf70c87f66b1d5f05c
Comon <comon928@gmail.com>
2023-03-11 14:24:20 +0300

    //

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,7 @@
 
 
 document.addEventListener('DOMContentLoaded', () =>{
-  var socket = io.connect('https://' + document.domain + ':'+ location.port);
+  var socket = io.connect('//' + document.domain + ':'+ location.port);
   
   // socket.on('connect', function(){
   //   socket.send({'msg': 'successfully connected'})
